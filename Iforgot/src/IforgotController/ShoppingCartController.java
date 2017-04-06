0b0adfbde2d7e40a22c6dbdc8564759d2bfae6f8
josecovarrubias <josecovarrubias@unknown.domain>
2017-04-05 23:28:34 -0700

    week 14 fixed controller

--- Iforgot/src/IforgotController/ShoppingCartController.java
+++ Iforgot/src/IforgotController/ShoppingCartController.java
@@ -8,9 +8,6 @@ package IforgotController;
 import IforgotModel.ShoppingCartModel;
 import IforgotView.ShoppingCartView;
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 /**
  *
@@ -20,41 +17,22 @@ public class ShoppingCartController implements Serializable{
     
 
     public ShoppingCartController() {
-       
-       
-        System.out.println("Name your list: ");
-        Scanner nameList = new Scanner(System.in);
-        String nameListIs = nameList.nextLine();
 
-        System.out.println("what item to add? ");
-        Scanner nameItem = new Scanner(System.in);
-        String nameItemIs = nameItem.nextLine();
+        ShoppingCartModel loadCart = new ShoppingCartModel();
+        ShoppingCartView ViewMycartName = new ShoppingCartView(); 
 
-        System.out.println("How many? ");
-        Scanner itemNum = new Scanner(System.in);
-        int itemNumIs = itemNum.nextInt();
-
-    ShoppingCartModel loadCart = new ShoppingCartModel();
-
-            loadCart.setShoppingListName(nameListIs);
+            loadCart.setShoppingListName(ViewMycartName.namingCart());
             String lista=loadCart.getShoppingListName().toUpperCase();
 
-            loadCart.setItem(nameItemIs);
+            loadCart.setItem(ViewMycartName.addingStuff());
             String cosa=loadCart.getItem().toUpperCase();
 
-            loadCart.setHowMany(itemNumIs);
+            loadCart.setHowMany(ViewMycartName.numOfItems());
             int cuanto= loadCart.getHowMany();
 
-        Map itemsInList = new HashMap();
-        itemsInList.put(cosa, cuanto);
-        itemsInList.keySet();
-//        System.out.println("CART "+itemsInList.entrySet());
-
-    ShoppingCartView ViewMycartName = new ShoppingCartView(); 
-        
-        ViewMycartName.ShoppingCartName(lista);
-        ViewMycartName.ShoppingCartItem(cosa);
-        ViewMycartName.ShoppingCartQuantity(cuanto);
-    }
+            ViewMycartName.ShoppingCartName(lista);
+            ViewMycartName.ShoppingCartItem(cosa);
+            ViewMycartName.ShoppingCartQuantity(cuanto);
+        }
     
 }
--- Iforgot/src/IforgotView/ShoppingCartView.java
+++ Iforgot/src/IforgotView/ShoppingCartView.java
@@ -6,6 +6,7 @@
 package IforgotView;
 
 import java.io.Serializable;
+import java.util.Scanner;
 
 /**
  *
@@ -16,12 +17,30 @@ public class ShoppingCartView implements Serializable{
     public ShoppingCartView(){
         
     }
+    public String namingCart(){
+        System.out.println("Name your list: ");
+        Scanner nameList = new Scanner(System.in);
+        String nameListIs = nameList.nextLine();
+        return nameListIs;
+    }
+    public String addingStuff(){
+        System.out.println("what item to add? ");
+        Scanner nameItem = new Scanner(System.in);
+        String nameItemIs = nameItem.nextLine();
+        return nameItemIs;
+    }
+    public int numOfItems(){
+        System.out.println("How many? ");
+        Scanner itemNum = new Scanner(System.in);
+        int itemNumIs = itemNum.nextInt();
+        return itemNumIs;
+    }
     public String ShoppingCartName(String lista){
          System.out.println("Your list name is: "+ lista);
          return lista;
 }  
     public String ShoppingCartItem(String cosa){
-         System.out.println("You added the following item/items:\n "+cosa);
+         System.out.println("You added item/items: "+cosa);
          return cosa;
         }
     public int ShoppingCartQuantity(int cuanto){
