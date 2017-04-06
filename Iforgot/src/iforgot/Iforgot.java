3aa5d6e19782f5427cfaafbb8d68cdf16dd92576
jose covarrubias <jose covarrubias@Cov-toshiba>
2017-04-06 14:05:15 -0700

    comments

--- Iforgot/src/iforgot/Iforgot.java
+++ Iforgot/src/iforgot/Iforgot.java
@@ -13,7 +13,8 @@ public class Iforgot {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        // This line leads to the application controller, in charge of the 
+        // initial logic of the application. 
         ApplicationController startApp = new ApplicationController();
         startApp.start();
     }
